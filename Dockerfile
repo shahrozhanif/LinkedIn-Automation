24ffbfc12673ee51b35b3bb2fc3e6b8dfa9e7026
Muhammad Abubakar <shahrozhanif32@gmail.com>
2026-04-09 11:05:34 +0500

    Project Completed

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM python:3.11.6-alpine
+WORKDIR /app
+COPY . /app/
+RUN python -m pip install --upgrade pip==24.2
+RUN pip install -r requirements.txt
+EXPOSE 8000
+CMD ["uvicorn", "app:app", "--host", "0.0.0.0", "--port", "8000"]
--- main.py
+++ main.py
@@ -37,10 +37,19 @@ def get_post():
 @app.post("/approve")
 async def approve(payload: ApprovalPayload):
     import httpx
-    N8N_WEBHOOK = "http://n8n:5678/webhook-test/16723437-e7f7-419c-9a2d-8bef08a74489"
-    async with httpx.AsyncClient() as client:
-        await client.post(N8N_WEBHOOK, json=payload.dict())
-    return {"status": "forwarded"}
+    global latest_post
+
+    if payload.status == "rejected":
+        N8N_WEBHOOK = "n8n/webhook/f5323699-6a4f-474c-a0c7-9ab1bff6b7d3"
+        async with httpx.AsyncClient() as client:
+            await client.post(N8N_WEBHOOK, json=payload.model_dump())
+    
+    # Clear the post regardless of approve or reject
+    latest_post = {"title": "", "body": ""}
+    
+    return {"status": payload.status}
+    
+    return {"status": payload.status}
 
 # Serve the React frontend
 app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
