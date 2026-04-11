5a3c802622d0f66fc47a5b73d60f99650a95a74e
Muhammad Abubakar <shahrozhanif32@gmail.com>
2026-04-11 11:20:10 +0500

    Tested with containers on local machine

--- main.py
+++ main.py
@@ -40,14 +40,12 @@ async def approve(payload: ApprovalPayload):
     global latest_post
 
     if payload.status == "rejected":
-        N8N_WEBHOOK = "n8n/webhook/f5323699-6a4f-474c-a0c7-9ab1bff6b7d3"
+        N8N_WEBHOOK = "http://n8n:5678/webhook/f5323699-6a4f-474c-a0c7-9ab1bff6b7d3"
         async with httpx.AsyncClient() as client:
             await client.post(N8N_WEBHOOK, json=payload.model_dump())
     
     # Clear the post regardless of approve or reject
     latest_post = {"title": "", "body": ""}
-    
-    return {"status": payload.status}
     
     return {"status": payload.status}
 
